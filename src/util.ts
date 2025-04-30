1106ff2584eb4cdd21461312b1f3f1d9199610ff
xf <imbooo@163.com>
2025-04-30 14:47:14 +0800

    feat(Util): 递归替换配置对象中的环境变量
    - 扩展 replacePlaceholdersWithEnvVars 方法以支持递归处理对象
    - 添加测试用例验证新功能- 优化类型定义和代码结构

--- src/util.ts
+++ src/util.ts
@@ -1,20 +1,23 @@
-export class Util{
-  static  replacePlaceholdersWithEnvVars(obj: Dict): Dict {
+export class Util {
+  static replacePlaceholdersWithEnvVars<T extends object>(obj: T):T {
     const rex = /\$\{([A-Z_]+)}/g;
-    function replaceValue(value: unknown) {
+
+    function replaceValue(value: string | object) {
       if (typeof value === "string") {
         value = value.replace(rex, (match, envVar) => {
           return process.env[envVar] ?? match;
         });
+      } else if (value && typeof value === "object") {
+        for (const k in value) {
+          if (Object.hasOwn(value, k)) {
+            value[k] = replaceValue(value[k]);
+          }
+        }
       }
 
       return value;
     }
 
-    for (const key in obj) {
-      obj[key] = replaceValue(obj[key]);
-    }
-
-    return obj;
+    return replaceValue(obj) as T;
   }
 }
--- test/util.spec.ts
+++ test/util.spec.ts
@@ -4,7 +4,11 @@ import { Util } from "../src/util";
 describe("Util", () => {
   it("replacePlaceholdersWithEnvVars", async () => {
     const config = {
-      host:"mysql-${ENV}.svc"
+      host:"mysql-${ENV}.svc",
+      test:["mysql-${ENV}.svc"],
+      tests:{
+        address:"mysql-${ENV}.svc"
+      }
     };
     const c = Util.replacePlaceholdersWithEnvVars(config);
     assert.isString(c.host);
